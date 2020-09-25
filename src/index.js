20806147b7e469424efe229b1e97ed0b90fd2847
mariel-delagarza <mariel.code@gmail.com>
2020-09-25 12:29:01 -0400

    Create main component and print hello world to screen to check functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+/*import './index.css';*/
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="game-board">
+          Hello World
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+);
