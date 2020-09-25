ffa2caf51108df7fbca8daf8e989587dcda1fd07
mariel-delagarza <mariel.code@gmail.com>
2020-09-25 14:54:29 -0400

    Finish setting up squares - update css so grid displays, has proper colors, etc. Create function so squares can be clicked and change color on hover.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/*for individual squares*/
+class Square extends React.Component {
+  selectSquare = () => {
+    this.props.selectSquare(this.props.row, this.props.col);
+  }
+
+  render() {
+    return (
+      <div 
+        className={this.props.squareClass}
+        id={this.props.id}
+        onClick={this.selectSquare}
+      />
+    )
+  }
+}
+
+/* Grid component finishes setting up the game board*/
+class Grid extends React.Component {
+  render() {
+    const width = (this.props.cols * 64);
+    let rowsArr = [];
+
+    let squareClass = "";
+    for (let i = 0; i < this.props.rows; i++) {
+      for (let j = 0; j < this.props.cols; j++) {
+        let squareId = i + "_" + j;
+
+        squareClass = this.props.fillGrid[i][j] ? "square on" : "square off";
+
+        rowsArr.push(
+          <Square 
+            squareClass = {squareClass}
+            key = {squareId}
+            squareId = {squareId}
+            row = {i}
+            col = {j} 
+            selectSquare = {this.props.selectSquare}
+          />
+        )
+      }
+    }
+
+    return (
+      <div className="grid" style={{width: width}}>
+        {rowsArr}
+      </div>
+    )
+  }
+}
+
+
 /*Main Component will hold all others*/
 class Main extends React.Component {
 
@@ -18,13 +70,26 @@ class Main extends React.Component {
     }
   }
 
+  selectSquare = (row, col) => {
+		let gridCopy = arrayClone(this.state.fillGrid);
+		gridCopy[row][col] = !gridCopy[row][col];
+		this.setState({
+			fillGrid: gridCopy
+		});
+	}
+
   // Render specifies what should actually display on the screen
   render() {
     return (
       <div className="main">
         <h1 id="title">Lights Out</h1>
         <div className="game-board">
-          hello
+          <Grid 
+            fillGrid={this.state.fillGrid}
+            rows={this.rows}
+            cols={this.cols}
+            selectSquare={this.selectSquare}
+          />
         </div>
         <div className="game-info">
           <h2>Number of Moves: {this.state.numberOfMoves}</h2>
@@ -32,6 +97,10 @@ class Main extends React.Component {
       </div>
     );
   }
+}
+
+function arrayClone(arr) {
+	return JSON.parse(JSON.stringify(arr));
 }
 
 // ========================================
